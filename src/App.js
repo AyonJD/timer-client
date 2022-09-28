75b698be048495ae1192fbe4d102f4d427ed7f7e
Ayon Jodder <ayonjodder177@gmail.com>
2022-09-29 02:42:49 +0600

    Date functionality done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [classes, setClasses] = useState([]);
+  const [lastUnlocked, setLastUnlocked] = useState(0);
 
   const getClasses = async () => {
     const response = await fetch('http://localhost:5000/classes');
@@ -13,7 +14,67 @@ function App() {
     getClasses();
   }, []);
 
-  console.log(classes);
+  //put is_unlock = true in every class after 24 hours
+
+
+  useEffect(() => {
+    const lastUnlocked = classes
+      .map((item, index) => (item.is_unlock === true ? index : null))
+      .filter((item) => item !== null)
+      .pop()
+
+    setLastUnlocked(lastUnlocked);
+  }, [lastUnlocked, classes]);
+
+  const updateClasses = async () => {
+    if (lastUnlocked) {
+      const classToUnlock = classes[lastUnlocked + 1];
+
+      const response = await fetch(`http://localhost:5000/classes/${classToUnlock._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ is_unlock: true }),
+      });
+      const data = await response.json();
+    }
+
+
+
+
+
+
+
+    // const response = await fetch('http://localhost:5000/classes', {
+    //   method: 'PUT',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(classes),
+    // });
+    // const data = await response.json();
+    // setClasses(data);
+  };
+  if (lastUnlocked) {
+    setTimeout(() => {
+      updateClasses();
+    }
+      , 1000 * 60 * 60 * 24);
+  }
+
+  var time_start = new Date();
+  var time_end = new Date();
+  var tomorrow_date = (time_start.getDate() + 1) + "-" + (time_start.getMonth() + 1) + "-" + time_start.getFullYear();
+  var value_start = "10:00:00".split(':');
+  var value_end = "10:00:00".split(':');
+
+  time_start.setHours(value_start[0], value_start[1], value_start[2], 0)
+  time_end.setHours(value_end[0], value_end[1], value_end[2], 0)
+
+  console.log(tomorrow_date)
+
+  // console.log(classes[lastUnlocked + 1]);
   return (
     <div className='container mx-auto my-10'>
       <div className="overflow-x-auto relative">
